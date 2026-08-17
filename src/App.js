56b15b1fa3d37e8f9709146b02c592d63b335624
Ch Sriram <chsriram2007@gmail.com>
2026-08-17 14:41:18 +0530

    Add project files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Fun from './Fun';
+
+function App() {
+  return (
+    <div className="App">
+<h1>Sriram Nec</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Fun.js
@@ -0,0 +1,10 @@
+import react from 'react';
+const Fun = () => {
+    return (
+        <div>
+            <h1>Fun Component</h1>
+        </div>
+    );
+};
+
+export default Fun;
